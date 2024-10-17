3dc82074082c672a2645b523a5f4427c66b73824
Pranay Rane <pranayrane029@gmail.com>
2024-10-17 13:46:50 +0530

    PathSum3

---
+++ src/Tree/BinaryTreeLevel3/PathSum3L437.java
@@ -0,0 +1,41 @@
+package Tree.BinaryTreeLevel3;
+
+public class PathSum3L437 {
+    int ans = 0;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null)
+            return 0;
+        solve(root, targetSum, 0);
+
+        pathSum(root.left, targetSum);
+
+        pathSum(root.right, targetSum);
+        return ans;
+    }
+
+    private void solve(TreeNode root, int targetSum, long sum) {
+        if (root == null)
+            return;
+        if (sum + root.val == targetSum)
+            ans++;
+
+        solve(root.left, targetSum, sum + root.val);
+        solve(root.right, targetSum, sum + root.val);
+    }
+}
+
+
+
+ class TreeNode {
+      int val;
+      TreeNode left;
+    TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+        this.right = right;
+    }
+ }
